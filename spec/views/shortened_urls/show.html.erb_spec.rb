344beb328d5da761973947603e2740374589f203
JacobGDG <jacob.greenwood@gmail.com>
2021-05-01 17:14:04 +0100

    Show short_url over uid on show page
    The use wont gain anything from seeing the uid on its own. The short_url
    is what they can for.

--- spec/views/shortened_urls/show.html.erb_spec.rb
+++ spec/views/shortened_urls/show.html.erb_spec.rb
@@ -4,17 +4,18 @@ require 'rails_helper'
 
 RSpec.describe 'shortened_urls/show', type: :view do
   before(:each) do
-    @shortened_url = assign(:shortened_url, ShortenedUrl.create!(
-                                              uid: 'Uid',
-                                              original_url: 'Original Url',
-                                              redirect_count: 2
-                                            ))
+    @shortened_url = assign(:shortened_url, shortened_url)
+  end
+  let(:shortened_url) do
+    ShortenedUrl.create!(
+      uid: 'Uid', original_url: 'https://www.example.com', redirect_count: 2
+    )
   end
 
   it 'renders attributes in <p>' do
     render
-    expect(rendered).to match(/Uid/)
-    expect(rendered).to match(/Original Url/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(/#{shortened_url.short_url}/)
+    expect(rendered).to match(/#{shortened_url.original_url}/)
+    expect(rendered).to match(/#{shortened_url.redirect_count}/)
   end
 end
