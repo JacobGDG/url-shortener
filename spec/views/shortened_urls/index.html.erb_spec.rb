b3d292cf2032ac70f01b39d9e200afaaf191732b
JacobGDG <jacob.greenwood@gmail.com>
2021-05-01 17:27:30 +0100

    Show short_url on index page over uid
    Same reason as the change to the Show page. The user won't gain anything
    from seeing the uid, so we should instead show them what they can for.
    The short_link.

--- spec/views/shortened_urls/index.html.erb_spec.rb
+++ spec/views/shortened_urls/index.html.erb_spec.rb
@@ -4,24 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'shortened_urls/index', type: :view do
   before(:each) do
-    assign(:shortened_urls, [
-             ShortenedUrl.create!(
-               uid: 'Uid',
-               original_url: 'Original Url',
-               redirect_count: 2
-             ),
-             ShortenedUrl.create!(
-               uid: 'Uid',
-               original_url: 'Original Url',
-               redirect_count: 2
-             )
-           ])
+    assign(:shortened_urls, [shortened_url1, shortened_url2])
   end
+  let(:shortened_url1) { create(:shortened_url) }
+  let(:shortened_url2) { create(:shortened_url) }
 
   it 'renders a list of shortened_urls' do
     render
-    assert_select 'tr>td', text: 'Uid'.to_s, count: 2
-    assert_select 'tr>td', text: 'Original Url'.to_s, count: 2
-    assert_select 'tr>td', text: 2.to_s, count: 2
+    assert_select 'a[href=?]', shortened_url1.short_url, count: 1
+    assert_select 'a[href=?]', shortened_url2.short_url, count: 1
+    assert_select 'a[href=?]', shortened_url1.original_url, count: 1
+    assert_select 'a[href=?]', shortened_url2.original_url, count: 1
   end
 end
