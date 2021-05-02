8dbd4080438c5392b65ebeb854eb0cfa4df78b71
JacobGDG <jacob.greenwood@gmail.com>
2021-05-02 12:02:11 +0100

    Validate the original_url contains a protocol
    URL validations is biiig task, and is very easy to over restrict. In
    this case the only thing we need is for a protocol and then the site
    will be able to correct link the user to the url. It is then on them to
    ensure they provide a link actually works.

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -6,6 +6,11 @@ class ShortenedUrl < ApplicationRecord
   belongs_to :user
 
   validates :uid, :original_url, presence: true
+  validates :original_url,
+            format: {
+              with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+              message: 'The protocol must be provided (http, https)'
+            }
 
   def short_url
     @short_url ||= ENV['SERVICE_URL'] + uid
